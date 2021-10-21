14610f7a5a413d76cda098e664f64635042bda0c
IagoCampideli <campidellisup@gmail.com>
2021-10-21 13:22:48 -0300

    fpo finalizado o projeto de recriar interface

--- js/owl/setup.js
+++ js/owl/setup.js
@@ -4,10 +4,10 @@ $('.owl-carousel').owlCarousel({
     nav:true,
     responsive:{
         0:{
-            items:1
+            items:1.3
         },
         600:{
-            items:3
+            items:3.3
         },
         1000:{
             items:5.3
